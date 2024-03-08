e402b225b66d4cc6cac14c0d92935efd60d358f6
Sam2022moon <104721736+Sam2022moon@users.noreply.github.com>
2024-03-09 00:59:34 +0900

    Initial commit

---
+++ nordichomes/urls.py
@@ -0,0 +1,11 @@
+
+from django.contrib import admin
+from django.urls import path
+
+from core.views import frontpage
+
+urlpatterns = [
+    path('',frontpage, name='frontpage'),
+    path('admin/', admin.site.urls),
+    
+]
